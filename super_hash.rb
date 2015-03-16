d9667422ece94a6538f3e29b221ab3134c877d75
Xander Miller <xander.miller@gmail.com>
2015-03-16 11:19:19 -0400

    first commit

---
+++ super_hash.rb
@@ -0,0 +1,241 @@
+@scf = { notes: {Enumerable => {commenters: {Enumerable => :empty_to_nil}}}, poll_id: :empty_to_nil }
+
+@sc = {:voter_id=>nil,
+ :civi_id=>nil,
+ :district_id=>1,
+ :poll_id=>nil,
+ :seq_id=>nil,
+ :elector_status=>nil,
+ :voting_status=>nil,
+ :first_name=>"Sterling",
+ :middle_name=>nil,
+ :last_name=>"Archer",
+ :notes=>{'1' => {commenters: {'bob' => "Bud", 'dick' => "wei", 'harry' => "ser"}, body: "Text"}, '2' => {commenters: {'bob' => "Bud", 'dick' => "wei", 'harry' => "ser"}, body: "Text"}, '3' => {}, '4' => {}},
+ :primary_email=>{"label"=>nil, "address"=>"sterling@isis.org"},
+ :other_emails=>[],
+ :primary_phone=>{"number"=>"4444444444", "label"=>nil, "textable"=>"0"},
+ :other_phones=>[{"number"=>"3333333333", "label"=>nil, "textable"=>"0"}],
+ :primary_address=>
+  {"unit_num"=>nil,
+   "bldg_num"=>"5",
+   "street_name"=>"Albert",
+   "street_type"=>"St",
+   "street_dir"=>nil,
+   "town"=>"Ottawa",
+   "mail_prov"=>"ON",
+   "postal_code"=>"K1K 1K1",
+   "bldg_num_sfx"=>nil,
+   "rural_addr_type"=>nil,
+   "rural_section"=>nil,
+   "rural_township"=>nil,
+   "rural_range"=>nil,
+   "rural_meridian"=>nil,
+   "rural_lot"=>nil,
+   "rural_concession"=>nil,
+   "rural_physical_addr"=>nil,
+   "mail_flag"=>nil,
+   "mail_addr1"=>nil,
+   "mail_addr2"=>nil,
+   "mail_place"=>nil,
+   "mail_postal_code"=>nil},
+ :other_addresses=>[],
+ :do_not_email=>false,
+ :do_not_phone=>false,
+ :do_not_mail=>false,
+ :tag_ids=>["c3a5c2b343c2b5c2a3c2a9c399000f82", "6f30c3a764c2a64c69c3962236001038", "4ec39fc28fc2b914c3b34fc3a400162d"],
+ :created_at=>1425322674,
+ :updated_at=>1426269496}
+
+@ss = {Enumerable => {
+          unit_num: :empty_to_nil,
+          bldg_num: :empty_to_nil,
+          street_name: :empty_to_nil,
+          street_type: :empty_to_nil,
+          street_dir: :empty_to_nil,
+          town: :empty_to_nil,
+          mail_prov: :empty_to_nil,
+          postal_code: :empty_to_nil,
+          bldg_num_sfx: :empty_to_nil,
+          rural_addr_type: :empty_to_nil,
+          rural_section: :empty_to_nil,
+          rural_township: :empty_to_nil,
+          rural_range: :empty_to_nil,
+          rural_meridian: :empty_to_nil,
+          rural_lot: :empty_to_nil,
+          rural_concession: :empty_to_nil,
+          rural_physical_addr: :empty_to_nil,
+          mail_flag: :empty_to_nil,
+          mail_addr1: :empty_to_nil,
+          mail_addr2: :empty_to_nil,
+          mail_place: :empty_to_nil,
+          mail_place_code: :empty_to_nil,
+          }}
+
+@sf = {first_name: :empty_to_nil, middle_name: :empty_to_nil, last_name: :empty_to_nil, district_id: :to_i, 
+        phones_attributes: {Enumerable => {
+          label: :empty_to_nil
+          }},
+        emails_attributes: {Enumerable => {
+          label: :empty_to_nil
+          }},
+        addresses_attributes: {Enumerable => {
+          unit_num: :empty_to_nil,
+          bldg_num: :empty_to_nil,
+          street_name: :empty_to_nil,
+          street_type: :empty_to_nil,
+          street_dir: :empty_to_nil,
+          town: :empty_to_nil,
+          mail_prov: :empty_to_nil,
+          postal_code: :empty_to_nil,
+          bldg_num_sfx: :empty_to_nil,
+          rural_addr_type: :empty_to_nil,
+          rural_section: :empty_to_nil,
+          rural_township: :empty_to_nil,
+          rural_range: :empty_to_nil,
+          rural_meridian: :empty_to_nil,
+          rural_lot: :empty_to_nil,
+          rural_concession: :empty_to_nil,
+          rural_physical_addr: :empty_to_nil,
+          mail_flag: :empty_to_nil,
+          mail_addr1: :empty_to_nil,
+          mail_addr2: :empty_to_nil,
+          mail_place: :empty_to_nil,
+          mail_place_code: :empty_to_nil,
+          }},
+        }
+
+@suf = {
+      first_name: :empty_to_nil,
+      last_name: :empty_to_nil,
+      email: :empty_to_nil,
+      phone: :empty_to_nil,
+      current_region_id: :empty_to_nil,
+      region_roles: { Enumerable => :empty_to_nil },
+      phones_attributes: { Enumerable => {
+        label: :empty_to_nil
+      }}
+    }
+
+@sua =  {:email=>"admin@example.com",
+ :phone=>nil,
+ :first_name=>"Elizabeth",
+ :last_name=>"May",
+ :phone_number=>nil,
+ :region_roles=>{"global"=>"admin", "5ac29fc2b0c2a8c381c380c2a6003908"=>nil, "5ac29fc2b0c2a8c381c380c2a600447a"=>nil},
+ :current_region_id=>"5ac29fc2b0c2a8c381c380c2a6003908",
+ :recent_contact_ids=>["58c39878c284c2bcc2b5384422001793", "0920c287c28330320475c3aac20001b4"],
+ :password_digest=>"$2a$10$hvYPFmgGwrYolMmj1OayX.cJpekKH2qfh9iT94hXlGjRIGnGdxJ.u",
+ :activation_digest=>"$2a$10$VViZ.ZurZH5OJVBftn8szeq0bCAgPg6Ne05AD7jNfspTba6Fwjqfu",
+ :activated=>true,
+ :activated_at=>nil,
+ :reset_digest=>nil,
+ :reset_sent_at=>nil,
+ :provider=>nil,
+ :uid=>nil,
+ :oauth_token=>nil,
+ :oauth_expires_at=>nil,
+ :sign_in_count=>1,
+ :last_sign_in_at=>1424893606,
+ :last_sign_in_ip=>"::1",
+ :accepted_terms_of_service=>true,
+ :created_at=>1424893254,
+ :updated_at=>1425323810}
+
+# def pathify_keys2(hsh, roots = nil, root = nil)
+#   root ||= []
+#   roots ||= [] # roots.is_a?(Array) && root.is_a?(Array) ? roots + hsh.keys.map {|k| root << k } : hsh.keys.map {|k| [k] }
+#   hsh.map do |key, value|
+#     root << key
+#     if value.is_a?(Hash)
+#       pathify_keys2(value, roots, root)
+#     else
+#       roots << root
+#       root = []
+#     end
+#   end
+#   roots
+# end
+
+# roots.each do |root|
+# if hsh[root.last].is_a?(Hash)
+# end 
+# end
+
+# def pathify_keys(hsh)
+#   hsh.map do |key, value|
+#     if value.is_a?(Hash)
+#       pathify_keys(value).map { |arr| ([key] + arr) }
+#     else
+#       [key]
+#     end
+#   end
+# end
+
+def pathify_keys(hsh)
+  hsh.inject([]) do |mem,(key, value)|
+    if value.is_a?(Hash)
+      pathify_keys(value).each { |arr| mem << ([key] + arr) }
+    else
+      mem << [key]
+    end
+    mem
+  end
+end
+
+def deep_fetch(*path)
+  path.reduce(self) do |mem, key|
+    mem[key] if mem
+  end
+end
+
+
+def get_by_keys(hash, keys)
+  keys.inject(hash) { |h, k| h.public_send(:[], k) }
+end
+
+
+def get_by_keys(hash, keys)
+  keys.inject(hash) { |h, k| h.public_send(:[], k) }
+end
+
+def set_by_keys(hash, keys, v)
+  last = keys[0..-2].inject(hash) { |h, k| h.public_send(:[], k) }
+  last.public_send(:[]=, keys[-1], v)
+  hash
+end
+
+def pathify_collections(filter, source)
+  pathified_filter = pathify_keys(filter)
+  pathified_source = pathify_keys(source)
+  includes_enum = ->(i){ i.include?(Enumerable)}
+  begin
+    pathified_filter.each do |filter_path|
+      if filter_path.include?(Enumerable)
+        expanded_paths = pathified_source.select { |source_path|
+          source_path.each_with_index.inject(true) { |memo, (element, index)| 
+            memo = false unless ((filter_path[index] == element) || (filter_path[index] == Enumerable))
+            print "#{element} #{filter_path[index]} #{memo} |\n"
+          }
+        }
+        pathified_filter.delete(filter_path)
+        pathified_filter += expanded_paths
+      end
+    end
+  end while pathified_filter.map(&includes_enum).include?(true)
+  pathified_filter
+end
+
+#Use filter path as a template for source path. So change the select condition.
+
+#sorted.each_with_index.inject(groups) do |group_container, (element,index)|
+
+def scrub_scalar(path, filter, source)
+  value = get_by_keys(source, path)
+  if value
+    action = get_by_keys(filter, path)
+    value.send(action)
+    set_by_keys(source, path, value)
+  end
+end
+
+
